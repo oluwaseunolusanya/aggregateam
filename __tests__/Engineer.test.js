472e45c0551fbe47aa0b6ecd7b9fdc663d675f83
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-01-21 21:09:04 +0000

    Created test cases for Engineer subclass.

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,18 +2,18 @@ const Engineer = require("../lib/Engineer");
 
 test("Can set GitHub account via constructor", () => {
   const testData = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testData);
-  expect(e.github).toBe(testData);
+  const employee = new Engineer("Foo", 1, "test@test.com", testData);
+  expect(employee.github).toBe(testData);
 });
 
 test("getRole() should return \"Engineer\"", () => {
   const testData = "Engineer";
-  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
-  expect(e.getRole()).toBe(testData);
+  const employee = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  expect(employee.getRole()).toBe(testData);
 });
 
 test("Can get GitHub username via getGithub()", () => {
   const testData = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testData);
-  expect(e.getGithub()).toBe(testData);
+  const employee = new Engineer("Foo", 1, "test@test.com", testData);
+  expect(employee.getGithub()).toBe(testData);
 });
