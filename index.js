a1cdb3482f408bb89f17d5f8275fa935f29dc9eb
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-01-23 21:35:47 +0000

    Created functions to instantiate manager, engineer and intern.

--- index.js
+++ index.js
@@ -8,29 +8,138 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
+//const render = require("./src/page-template.js");
 
 const team = [];
+const menuOption = "";
 
-const init = () => {
-    console.log("Welcome to Aggregateam, a program that generates a webpage that displays contact card of your team members.")
-    inquirer.
-    prompt([
+const createManager = () => {
+    inquirer
+    .prompt([
         {
             type:"input",
-            message: "Provide your email to address questions about the project.",
+            message: "Name:",
+            name: "name"
+        },
+        {
+            type:"input",
+            message: "Employee ID:",
+            name: "id"
+        },
+        {
+            type:"input",
+            message: "Email Address:",
             name: "email"
         },
         {
             type:"input",
-            message: "Author:",
-            name: "author"
-        },
-
+            message: "Office Number:",
+            name: "officeNumber"
+        }
     ])
-    .then((response) => {
-
+    .then((data) => {
+        const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+        team.push(manager);
     });
+    menuOption = showMenu();
+};
+
+const showMenu = () => {
+    inquirer
+    .prompt([
+        {
+            type:"list",
+            message: "Select next action",
+            name: "option",
+            choices:[
+                "Add an engineer",
+                "Add an intern",
+                "Finish building the team",
+            ]
+        }
+    ])
+    .then((data) => {
+        return data.option;
+    });
+};
+
+const createEngineer = () => {
+    inquirer
+    .prompt([
+        {
+            type:"input",
+            message: "Engineer's Name:",
+            name: "name"
+        },
+        {
+            type:"input",
+            message: "Employee ID:",
+            name: "id"
+        },
+        {
+            type:"input",
+            message: "Email Address:",
+            name: "email"
+        },
+        {
+            type:"input",
+            message: "GitHub Username:",
+            name: "github"
+        }
+    ])
+    .then((data) => {
+        const engineer = new Engineer(data.name, data.id, data.email, data.github);
+        team.push(engineer)
+    })
+};
+
+const createIntern = () => {
+    inquirer
+    .prompt([
+        {
+            type:"input",
+            message: "Intern's Name:",
+            name: "name"
+        },
+        {
+            type:"input",
+            message: "Employee ID:",
+            name: "id"
+        },
+        {
+            type:"input",
+            message: "Email Address:",
+            name: "email"
+        },
+        {
+            type:"input",
+            message: "School:",
+            name: "school"
+        }
+    ])
+    .then((data) => {
+        const intern = new Intern(data.name, data.id,data.email, data.school);
+        team.push(intern)
+    })
+};
+
+
+const init = () => {
+    console.log(`Welcome to Aggregateam, a program that generates a webpage that displays contact card of your team members.
+Enter the team's manager details.`);
+    createManager();
+
+    // switch (option) {
+    //     case "Add an engineer":
+    //         createEngineer();            
+    //         break;
+                    
+    //     case "Add an intern":
+    //         createIntern(); 
+    //         break;
+    //     default:
+    //         break;
+    // }
 };
 
 // Initialise programm
